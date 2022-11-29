1b6242dbc679428c3c7b1e4c7bad389719e9e8cb
bayzhen <1521085156@qq.com>
2022-11-29 21:12:20 +0800

    Initial commit

---
+++ Source/RLAL/RLALGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RLALGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RLAL_API ARLALGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
